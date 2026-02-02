475fbc9684d283ea35e0dc166bb40a6c10caef1c
elshoraamira <elshoraamira@gmail.com>
2026-02-02 17:24:29 +0200

    init git

---
+++ ems/companies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    department_count = models.PositiveIntegerField(default=0)
+    employee_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.name
