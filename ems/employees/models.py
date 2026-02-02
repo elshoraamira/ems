974d4b9854307cd4dfd078a95fa933ea61762c9c
elshoraamira <elshoraamira@gmail.com>
2026-02-03 01:31:15 +0200

    employees model refinement

--- ems/employees/models.py
+++ ems/employees/models.py
@@ -11,8 +11,8 @@ class Employee(models.Model):
         ('rejected', 'Not Accepted'),
     ]
 
-    company = models.ForeignKey(Company, on_delete=models.PROTECT)
-    department = models.ForeignKey(Department, on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, on_delete=models.PROTECT, related_name='employees')
+    department = models.ForeignKey(Department, on_delete=models.PROTECT, related_name='employees')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES)
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
@@ -22,17 +22,22 @@ class Employee(models.Model):
     hired_on = models.DateField(null=True, blank=True) # Can be null if not hired yet
 
     def clean(self):
-        if self.department.company_id != self.company_id:
-            raise ValidationError(
-                "Selected department does not belong to the selected company."
-            )
+        if self.department and self.company:
+            if self.department.company_id != self.company_id:
+                raise ValidationError(
+                    "Selected department does not belong to the selected company."
+                )
         
         if self.status == 'hired' and not self.hired_on:
             raise ValidationError(
                 "Hired date must be provided when employee status is 'Hired'."
             )
     
+    @property
     def days_employed(self):
         if self.status == 'hired' and self.hired_on:
             return (date.today() - self.hired_on).days
         return None
+    
+    def __str__(self):
+        return f"{self.name} ({self.company.name})"
