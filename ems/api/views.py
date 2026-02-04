45a0ada1f26db9acdf491d370b751b31e3f18011
elshoraamira <elshoraamira@gmail.com>
2026-02-05 01:47:08 +0200

    adding APIs for the models

--- ems/api/views.py
+++ ems/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from employees.models import Employee
+from .serializers import EmployeeSerializer
+
+
+class EmployeeListView(ListAPIView):
+    serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        # Superuser: see all
+        if user.is_superuser:
+            return Employee.objects.all()
+
+        # Manager: see own department
+        if user.groups.filter(name='Manager').exists():
+            return Employee.objects.filter(
+                department=user.employee.department
+            )
+
+        # Employee: see self only
+        return Employee.objects.filter(user=user)
