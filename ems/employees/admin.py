19adaa631c0b38acfe0751f5c9efd1b4bbe057e7
elshoraamira <elshoraamira@gmail.com>
2026-02-04 23:28:13 +0200

    adding filtration for each group/role

--- ems/employees/admin.py
+++ ems/employees/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Employee
 # Register your models here.
-admin.site.register(Employee)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        if request.user.is_superuser:
+            return qs
+        elif request.user.groups.filter(name='Manager').exists():
+            return qs.filter(department=request.user.employee.department)
+        else:
+            return qs.filter(user=request.user)
+
+
+admin.site.register(Employee, EmployeeAdmin)
