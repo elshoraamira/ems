4e1de430324f12bd474b3ac6c9a52678993cc2ed
elshoraamira <elshoraamira@gmail.com>
2026-02-04 23:49:57 +0200

    creating apis project

--- ems/ems/settings.py
+++ ems/ems/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'companies.apps.CompaniesConfig',
-    'employees.apps.EmployeesConfig'
+    'employees.apps.EmployeesConfig',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
