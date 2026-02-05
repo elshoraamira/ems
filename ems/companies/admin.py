7f3627e58ac9e688868f5a047a4a39da89caee00
elshoraamira <elshoraamira@gmail.com>
2026-02-05 03:01:06 +0200

    adding the readonly fields to admin for company and department model

--- ems/companies/admin.py
+++ ems/companies/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Company, Department
 # Register your models here.
-admin.site.register(Company)
-admin.site.register(Department)
+class CompanyAdmin(admin.ModelAdmin):
+
+    readonly_fields = [
+        'departments_count',
+        'employees_count',
+    ]
+
+
+admin.site.register(Company, CompanyAdmin)
+class DepartmentAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        'employees_count',
+    ]
+
+
+admin.site.register(Department, DepartmentAdmin)
