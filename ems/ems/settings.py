f1f8445fed17f4e2b9e588c57c574257249fc400
elshoraamira <elshoraamira@gmail.com>
2026-02-24 04:00:30 +0200

    debug line

--- ems/ems/settings.py
+++ ems/ems/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-#mso+7%k1-5_0ns8poq)qr0$to)b(8n57_=e67fs@1x28ko3ux'
 
+DEBUG = True
 ALLOWED_HOSTS = []
 
 
